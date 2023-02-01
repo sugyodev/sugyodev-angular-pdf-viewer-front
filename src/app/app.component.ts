e9ed9c0b3cba262ff6e78a3e0a0b0746dc68caf6
sg <sg.guru1030@gmail.com>
2023-02-02 04:48:49 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  totalPages = 0;
+  page = this.totalPages ? 1 : 0;
   title = 'angular-pdf-viewer-example';
+  pdfSrc = "";
+  // pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+  constructor(private httpClient: HttpClient) { }
+  nextPage() {
+    this.page++;
+  }
+
+  sendGetRequest() {
+    this.httpClient.get('http://localhost:8080/getPDF').subscribe((responseBody : any) => {
+      if(responseBody){
+        this.pdfSrc = responseBody.pdfUrl;
+      }
+    });
+  }
+
+  prevPage() {
+    this.page--;
+  }
+
+  afterLoadComplete(pdfData: any) {
+    this.totalPages = pdfData.numPages;
+    this.page = 1
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PdfViewerModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
